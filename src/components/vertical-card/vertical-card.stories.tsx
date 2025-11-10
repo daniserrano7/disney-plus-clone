86f00520b09684f448201ab6a950f42da8104d9e
Dani <danielserrano.contacto@gmail.com>
2025-11-10 18:22:32 +0100

    feat: vertical card info

--- src/components/vertical-card/vertical-card.stories.tsx
+++ src/components/vertical-card/vertical-card.stories.tsx
@@ -19,3 +19,16 @@ export const VerticalCard: Story = {
     alt: "Coppola",
   },
 };
+
+export const VerticalCardWithInfo: Story = {
+  args: {
+    url: "https://www.disneyplus.com/",
+    img: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AFAF32C312CAEF54B8D1E119D4F7E95799FD10520CEE1453BD90107B74CE9FD0/badging?width=400&aspectRatio=0.71&format=jpeg&label=staroriginal",
+    alt: "Coppola",
+    info: {
+      pegi: "18+",
+      year: "2021",
+      tags: ["Action", "Adventure"],
+    },
+  },
+};
--- src/components/vertical-card/vertical-card.tsx
+++ src/components/vertical-card/vertical-card.tsx
@@ -1,10 +1,21 @@
-export const VerticalCard = ({ url, img, alt }: Props) => {
+import { Pegi } from "@/components/pegi";
+import cx from "classix";
+
+export const VerticalCard = ({ url, img, alt, info }: Props) => {
   return (
     <a
       href={url}
-      className="block h-[338px] w-[240px] rounded shadow-card outline outline-0 transition-transform duration-300 ease-out hover:scale-105 hover:outline-[3px] hover:outline-offset-[3px] hover:outline-border"
+      className="relative block h-[338px] w-[240px] rounded shadow-card outline-0 transition-transform duration-300 ease-out hover:scale-105 hover:outline-[3px] hover:outline-offset-[3px] hover:outline-border"
     >
       <img src={img} alt={alt} className="rounded" />
+      {info ? (
+        <div className="absolute bottom-0 left-0 right-0 flex items-center gap-2">
+          <Pegi value={info.pegi} />
+          <div className="flex gap-1 text-xs text-font-subtlest">
+            <span>{info.year}</span> <span>•</span> <span>{info.tags.join(", ")}</span>
+          </div>
+        </div>
+      ) : null}
     </a>
   );
 };
@@ -13,4 +24,9 @@ interface Props {
   url: string;
   img: string;
   alt: string;
+  info?: {
+    pegi: string;
+    year: string;
+    tags: string[];
+  };
 }
